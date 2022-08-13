a9f006b85fdc003eccc8b79ad7b598b9aa424731
kengo78 <fulken35@gmail.com>
2022-08-13 16:40:12 +0900

    立ち上げまで完成

--- KakeiboApp/settings.py
+++ KakeiboApp/settings.py
@@ -17,7 +17,7 @@ env.read_env('.env')
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG')
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ["127.0.0.1:8000", "0.0.0.0"]
 # Application definition
 
 INSTALLED_APPS = [
@@ -69,7 +69,7 @@ DATABASES = {
     'NAME': 'Kakeibo',
     'USER': 'root',
     'PASSWORD': 'u585304g',
-    # 'HOST': 'db',
+    'HOST': 'db',
     'POST': '3306',
     }
 }
