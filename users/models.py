d34d0d960d31123798e590475923967cedc60e42
kengo78 <fulken35@gmail.com>
2022-09-07 18:37:26 +0900

    models.py

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.core.mail import send_mail
-# from django.utils import timezone
+from django.utils import timezone
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils.translation import gettext_lazy as _
 
