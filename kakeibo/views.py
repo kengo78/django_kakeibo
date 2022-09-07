a3e72218c23af84efaf77e7505fc8516c5a0ee66
kengo78 <fulken35@gmail.com>
2022-09-07 20:11:14 +0900

    検索機能

--- kakeibo/views.py
+++ kakeibo/views.py
@@ -3,7 +3,60 @@ from django.views import generic
 from .models import Payment, PaymentCategory, Income, IncomeCategory
 
 
+from django.views import generic
+from .models import Payment, PaymentCategory, Income, IncomeCategory
+from .forms import PaymentSearchForm  # 追加
+
+
 class PaymentList(generic.ListView):
     template_name = 'kakeibo/payment_list.html'
     model = Payment
     ordering = '-date'
+
+    # 追加
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.form = form = PaymentSearchForm(self.request.GET or None)
+
+        if form.is_valid():
+            year = form.cleaned_data.get('year')
+            # 何も選択されていないときは0の文字列が入るため、除外
+            if year and year != '0':
+                queryset = queryset.filter(date__year=year)
+
+            # 何も選択されていないときは0の文字列が入るため、除外
+            month = form.cleaned_data.get('month')
+            if month and month != '0':
+                queryset = queryset.filter(date__month=month)
+
+            # 〇〇円以上の絞り込み
+            greater_than = form.cleaned_data.get('greater_than')
+            if greater_than:
+                queryset = queryset.filter(price__gte=greater_than)
+            
+            # 〇〇円以下の絞り込み
+            less_than = form.cleaned_data.get('less_than')
+            if less_than:
+                queryset = queryset.filter(price__lte=less_than)
+            
+            # キーワードの絞り込み
+            key_word = form.cleaned_data.get('key_word')
+            if key_word:
+                # 空欄で区切り、順番に絞る、and検索
+                if key_word:
+                    for word in key_word.split():
+                        queryset = queryset.filter(description__icontains=word)
+            
+            # カテゴリでの絞り込み
+            category = form.cleaned_data.get('category')
+            if category:
+                queryset = queryset.filter(category=category)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # search formを渡す
+        context['search_form'] = self.form
+
+        return context
