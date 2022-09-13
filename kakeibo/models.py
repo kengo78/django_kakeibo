044b8f63baba7c7dd373161d64beef7df3ef8901
kengo78 <fulken35@gmail.com>
2022-09-14 00:36:48 +0900

    model形成

--- kakeibo/models.py
+++ kakeibo/models.py
@@ -25,7 +25,15 @@ class Income(models.Model):
     price = models.IntegerField('金額')
     category = models.ForeignKey(IncomeCategory, on_delete=models.PROTECT, verbose_name='カテゴリ')
     description = models.TextField('概要', null=True, blank=True)
+    
+class BankCategory(models.Model):
+    name = models.CharField('カテゴリ名', max_length=32)
+
+    def __str__(self):
+        return self.name
 
 class Rest(models.Model):
-    sum_rest = models.IntegerField('残り金額')
+    rest = models.IntegerField('残り金額')
+    date = models.DateField('日付')
+    bank_category = models.ForeignKey(BankCategory, on_delete=models.PROTECT,verbose_name='銀行')
     
